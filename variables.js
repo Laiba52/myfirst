874db0bc25beb688bdbdb0965a10453f28f1a83e
Laiba52 <kanwallaiba52@gmail.com>
2023-09-21 10:29:07 +0500

    myfirst

---
+++ variables.js
@@ -0,0 +1,62 @@
+//var num3 = 2;
+//if (num3 + num2 === 6){
+
+ //   alert(num2 + "+" + num3 + "=" , num3+num2);}
+//else {
+  //  alert(num2 + "+" + num1 + "=", num2+num1 )
+//}
+// if = 1, tru , str value "hellow "
+// else false 
+//var bool = true;
+//if (bool){
+//    alert("im if num 1")
+//}
+//else {
+//    alert("im else num 1")
+//}
+//if (!bool){
+//    alert("im if 2")
+//}
+//else{ 
+//    alert("im else 2")
+//
+//var x = promt("insrt ur ity ");
+
+//if (!(x===karahi)) {
+//  alert(" allow")
+// if (x=== islamaba) {
+// alert("apital of pak")}
+//  var city = prompt("enter your city");
+//  var gender = prompt("enter your gender");
+//  var appliedFor = prompt("did you apply for dubai?:yes ,no");
+//  var city2 = ["karachi","islamabad","lahore"];
+//  if ((city === city2[0] ||city2[1] || city2[2]) && gender === "male" && appliedFor=== "yes"){
+//      console.log("yes you are allowed")
+//  }
+//  else {
+//      console.log("not allowed")
+//  }
+//var math = +prompt("enter your maths marks");
+//var eng = +prompt("enter your english marks");
+//var sci = +prompt("enter your science num");
+//var total = sci+ math + eng; 
+//var percentage =(total/300)*100;                          
+//console.log(  "your percentage is" ,percentage)  
+
+// var percentage = (total/300)*100;
+// console.log( percentage)
+
+ 
+//if ( 100>= percentage && percentage >= 80){
+//   console.log("you got A+");
+
+//}
+// else if (79 >= percentage && percentage >= 70){
+//    console.log("you got A");
+//}
+// else if ( 69 >= percentage && percentage >= 40 ){
+//   console.log("you got B");
+//} 
+//else {
+//    console.log("you are fail")
+//}
